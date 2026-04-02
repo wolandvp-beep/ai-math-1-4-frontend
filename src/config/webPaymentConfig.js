6c9c5433bc1cbd4e68ed800af28c6ef38f87df6c
wolandvp-beep <woland.vp@gmail.com>
2026-04-02 12:40:36 +1000

    Delete src directory

--- src/config/webPaymentConfig.js
+++
@@ -1,5 +0,0 @@
-export const WEB_PAYMENT_CONFIG = {
-  billingPortalUrl: 'https://pay.your-domain.example/reshayka',
-  successReturnUrl: 'reshayka://billing/success',
-  cancelReturnUrl: 'reshayka://billing/cancel'
-};
