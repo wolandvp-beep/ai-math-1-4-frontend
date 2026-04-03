883de424ab78e950923ff91bf9e64d2177498be3
wolandvp-beep <woland.vp@gmail.com>
2026-04-03 10:47:43 +1000

    Delete src directory

--- src/billing/storeBillingAdapter.js
+++
@@ -1,7 +0,0 @@
-import { ENV } from '../config/env.js';
-import { mockStoreBillingAdapter } from './mockStoreBillingAdapter.js';
-import { remoteStoreBillingAdapter } from './remoteStoreBillingAdapter.js';
-
-export function getStoreBillingAdapter() {
-  return ENV.API_MODE === 'remote' ? remoteStoreBillingAdapter : mockStoreBillingAdapter;
-}
