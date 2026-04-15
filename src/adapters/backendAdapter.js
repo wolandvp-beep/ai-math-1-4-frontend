a90abfa07f1fee46f596e62e8da4db8d21cbda9f
wolandvp-beep <woland.vp@gmail.com>
2026-04-15 16:07:38 +1000

    Delete src directory

--- src/adapters/backendAdapter.js
+++
@@ -1,7 +0,0 @@
-import { ENV } from '../config/env.js';
-import { remoteBackendAdapter } from './remoteBackendAdapter.js';
-import { mockBackendAdapter } from './mockBackendAdapter.js';
-
-export function getBackendAdapter() {
-  return ENV.API_MODE === 'remote' ? remoteBackendAdapter : mockBackendAdapter;
-}
